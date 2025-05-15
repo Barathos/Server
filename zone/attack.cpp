6e0d3b38209af1d43f4003303726590ddbe78f04
David H Bross <dbrosswsu@gmail.com>
2025-05-15 11:00:56 -0500

    Fix Auto-Hater extended target entries not clearing under some circumstances (#171)

--- zone/attack.cpp
+++ zone/attack.cpp
@@ -2609,7 +2609,7 @@ bool NPC::Death(Mob* killer_mob, int64 damage, uint16 spell, EQ::skills::SkillTy
 
 	auto* killer = GetHateDamageTop(this);
 
-	entity_list.RemoveFromTargets(this, p_depop);
+	entity_list.RemoveFromTargets(this, true);
 
 	if (p_depop) {
 		return false;
